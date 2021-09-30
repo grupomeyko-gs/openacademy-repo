41940bdb6ac43f647e9bbc29c74cf35ca7f3a6e3
Daniel Martinez <gerenciasistemas@grupomeyko.hn>
2021-09-29 18:00:09 -0600

    initial commint

---
+++ openacademy/models/models.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models, fields, api
+
+
+class Course(models.Model):
+    _name = 'openacademy.course'
+#     _description = 'openacademy.openacademy'
+
+    name = fields.Char(string="Title",required=True)
+#     value = fields.Integer()
+#     value2 = fields.Float(compute="_value_pc", store=True)
+    description = fields.Text()
+#
+#     @api.depends('value')
+#     def _value_pc(self):
+#         for record irecord.value2 = float(record.value) / 100

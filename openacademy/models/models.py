71809d532ba3a536ce11bc02a0dd39c6d76fb115
Daniel Martinez <gerenciasistemas@grupomeyko.hn>
2021-10-03 14:15:02 -0600

    follow commit

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -5,7 +5,7 @@ from odoo import models, fields, api
 
 class Course(models.Model):
     _name = 'openacademy.course'
-#     _description = 'openacademy.openacademy'
+    _description = 'Cursos disponibles para impartir'
 
     name = fields.Char(string="Title",required=True)
 #     value = fields.Integer()

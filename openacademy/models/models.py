297301745b6bc434300547b63294772068b4143c
Daniel Martinez <gerenciasistemas@grupomeyko.hn>
2021-10-05 17:19:54 -0600

    Onchange y restricciones

--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -1,21 +1,36 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, exceptions
 
 class Course(models.Model):
     _name = 'openacademy.course'
     _description = 'Courses to the public'
 
     name = fields.Char(string="Title",required=True)
-#     value = fields.Integer()
-#     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
     responsible_id = fields.Many2one('res.users', ondelete='set null', string='Responsible', index=True)
     session_ids = fields.One2many('openacademy.session', 'course_id', string='Sessions')
-#
-#     @api.depends('value')
-#     def _value_pc(self):
-#         for record irecord.value2 = float(record.value) / 100
+
+    def copy(self, default=None):
+        default = dict(default or {})
+        copied_count = self.search_count(
+                [("name","=like",u"Copy of {}%".format(self.name))])
+        if not copied_count:
+            new_name = u"Copie of {}".format(self.name)
+        else:
+            new_name = u"Copie of {} ({})".format(self.name, copied_count)
+
+        default["name"] = new_name
+        return super(Course, self).copy(default)
+
+    _sql_constraints = [
+        ('name_description_check',
+         'CHECK(name != description)',
+         "The tittle of this course should not be the description"),
+        ('name_unique',
+         'UNIQUE(name)',
+         'The course title must be unique'),
+    ]
 
 class Session(models.Model):
     _name = 'openacademy.session'
@@ -27,7 +42,7 @@ class Session(models.Model):
     seats = fields.Integer(string="Number of seats")
     active = fields.Boolean(default=True)
 
-    instructor_ids = fields.Many2one('res.partner', string='Instructor',
+    instructor_id = fields.Many2one('res.partner', string='Instructor',
             domain=['|',('instructor','=',True), ('category_id.name','ilike','Teacher')])
     course_id = fields.Many2one('openacademy.course',ondelete='cascade',string='Course', required=True)
     attendee_ids = fields.Many2many('res.partner', string='Attendees')
@@ -58,3 +73,8 @@ class Session(models.Model):
                         'message' : 'Increase seats or remove attendees',
                         },
             }
+    @api.constrains('instructor_id','attendee_ids')
+    def _check_instructor_not_in_attendees(self):
+        for r in self:
+            if r.instructor_id and r.instructor_id in r.attendee_ids:
+                raise exceptions.ValidationError("A session's instructor can't be an attendee")

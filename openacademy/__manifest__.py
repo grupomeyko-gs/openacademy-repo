64690f715aed4cadc268a95c90f213ce92c3dc57
Daniel Martinez <gerenciasistemas@grupomeyko.hn>
2021-10-05 10:55:14 -0600

    tutorial onchange

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -27,9 +27,10 @@
         'security/ir.model.access.csv',
         'views/views.xml',
         'views/templates.xml',
-        'views/openacademy.xml'
+        'views/openacademy.xml',
+        'views/partner.xml'
     ],
-    # only loaded in demonstration mode
+    # only loaded in dEmonstration mode
     'demo': [
         'demo/demo.xml',
     ],
--- openacademy/models/models.py
+++ openacademy/models/models.py
@@ -2,16 +2,59 @@
 
 from odoo import models, fields, api
 
-
 class Course(models.Model):
     _name = 'openacademy.course'
-    _description = 'Cursos disponibles para impartir'
+    _description = 'Courses to the public'
 
     name = fields.Char(string="Title",required=True)
 #     value = fields.Integer()
 #     value2 = fields.Float(compute="_value_pc", store=True)
     description = fields.Text()
+    responsible_id = fields.Many2one('res.users', ondelete='set null', string='Responsible', index=True)
+    session_ids = fields.One2many('openacademy.session', 'course_id', string='Sessions')
 #
 #     @api.depends('value')
 #     def _value_pc(self):
 #         for record irecord.value2 = float(record.value) / 100
+
+class Session(models.Model):
+    _name = 'openacademy.session'
+    _description = 'Sessions to the public'
+
+    name = fields.Char(required=True)
+    start_date = fields.Date()
+    duration = fields.Float(digits=(6,2), help="Duration in days")
+    seats = fields.Integer(string="Number of seats")
+    active = fields.Boolean(default=True)
+
+    instructor_ids = fields.Many2one('res.partner', string='Instructor',
+            domain=['|',('instructor','=',True), ('category_id.name','ilike','Teacher')])
+    course_id = fields.Many2one('openacademy.course',ondelete='cascade',string='Course', required=True)
+    attendee_ids = fields.Many2many('res.partner', string='Attendees')
+
+    taken_seats = fields.Float(string="Taken Seats",compute="_taken_seats")
+
+    @api.depends('seats','attendee_ids')
+    def _taken_seats(self):
+        for r in self:
+            if not r.seats:
+                r.taken_seats = 0.0
+            else:
+                r.taken_seats = 100.00 * len(r.attendee_ids) / r.seats
+
+    @api.onchange('seats','attendee_ids')
+    def _verify_valid_seats(self):
+        if self.seats < 0:
+            return {
+                    'warning' : {
+                        'title' : 'Incorrect seats value',
+                        'message' : 'The number of available seats may not be a negative',
+                    },
+            }
+        if self.seats < len(self.attendee_ids):
+            return {
+                    'warning' : {
+                        'title' : 'Too many attendees',
+                        'message' : 'Increase seats or remove attendees',
+                        },
+            }
